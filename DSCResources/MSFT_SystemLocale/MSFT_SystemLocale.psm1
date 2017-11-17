15f0e0a42e915e3eed653b712c1e73f80243cdee
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-11-18 10:30:33 +1300

    Removed unesseccary lines

--- DSCResources/MSFT_SystemLocale/MSFT_SystemLocale.psm1
+++ DSCResources/MSFT_SystemLocale/MSFT_SystemLocale.psm1
@@ -1,8 +1,3 @@
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '', Scope = 'Function')]
-param
-(
-)
-
 $modulePath = Join-Path -Path (Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent) -ChildPath 'Modules'
 
 # Import the Networking Resource Helper Module
